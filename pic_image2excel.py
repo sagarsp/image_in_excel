15a26cbf0e8e698bc58d33784002a71a6a06dc15
Sagar Patel <sagarpatel.9556@gmail.com>
2022-04-21 19:45:41 +0530

    Update pic_image2excel.py

--- pic_image2excel.py
+++ pic_image2excel.py
@@ -4,25 +4,25 @@ from openpyxl.styles import PatternFill
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
+
 #Open Image and destination XLS file
 img = plt.imread("pic.tif")
 dest_filename = 'pic_excel.xlsx'
-rows, cols, rgb = np.shape(img)
 R, G, B = img[:,:,0], img[:,:,1], img[:,:,2]
 wb = Workbook()
 ws = wb.active
+
 # Set the width and height of cells (1 width and 5.5 height is square to replicate pixel)
 for i in range(len(R)):
     ws.column_dimensions[get_column_letter(i+1)].width = 1
     ws.row_dimensions[i+1].height = 5.5
+    
 # Traverse through the metrix of pixes. Extract Red, Green and Blue value and set the fill the cell at i, j location with that color.
 for i in range(len(R)):
     for j in range(len(R[i])):
-        R_comp = R[i][j]
-        G_comp = G[i][j]
-        B_comp = B[i][j]
-        rgb = (R_comp,G_comp,B_comp)
-        final = bytes.hex(struct.pack('BBB',*rgb))
+        rgb = (R[i][j],G[i][j],B[i][j])
+        rgb_hex = bytes.hex(struct.pack('BBB',*rgb))
         cell_id = get_column_letter(j+1)+str(i+1)
-        ws[cell_id].fill = PatternFill("solid", start_color=final)
+        ws[cell_id].fill = PatternFill("solid", start_color=rgb_hex)
+        
 wb.save(filename = dest_filename)
